b0b9133951bea433f121eb2af61456f71930ffc8
Patryk <56590487+Pattal@users.noreply.github.com>
2023-09-25 16:09:03 +0200

    Created game manager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Game Manager", menuName = "ScriptableObjects/Managers/GameManager")]
+public class GameManager : ScriptableObject, IManager, IStartable
+{
+    public Bootstrapper Bootstrapper { get; set; }
+
+    [SerializeField] GameAreaManager _areaManager;
+    [SerializeField] EndWindowUIControllerVariable _endWindowUIController;
+    [SerializeField] GameModeSelectorUIControllerVariable _gameModeSelectorUIController;
+
+    public void CustomStart()
+    {
+        _areaManager.OnLastAgentExist += () => ShowEndMenu();
+    }
+
+    public void Reset()
+    {
+        _areaManager.Reset();
+    }
+
+    public void MainMenuButtonClicked()
+    {
+        _gameModeSelectorUIController.Value.gameObject.SetActive(true);
+        Reset();
+    }
+
+    private void ShowMainMenu()
+    {
+
+    }
+
+    private void ShowEndMenu()
+    {
+        _endWindowUIController.Value.gameObject.SetActive(true);
+    }
+
+}
+
