866ec5e7c7cc8e741de9fbc9bfe4c44fb3373793
Patryk <56590487+Pattal@users.noreply.github.com>
2023-09-25 17:05:51 +0200

    Added game variant script for easier game design

---
+++ Assets/Scripts/Managers/GameVariant.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameVariant", menuName = "ScriptableObjects/GameVariant")]
+public class GameVariant : ScriptableObject
+{ 
+    [field: SerializeField] public int NumberOfAgents { get; private set; }
+    [field: SerializeField] public GameVariantUI UIPrefab { get; private set; }
+}
+
+
---
+++ Assets/Scripts/UI/GameVariantUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class GameVariantUI : MonoBehaviour
+{
+    public string Quantinty { set => _quantintyText.text = value; }
+    public GameVariant GameVariant;
+    public Toggle Toggle;
+    public Action<int> OnToggleSelected;
+
+    [SerializeField] private TextMeshProUGUI _quantintyText;
+}
+
